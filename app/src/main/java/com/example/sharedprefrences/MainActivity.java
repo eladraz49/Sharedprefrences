ff007966b2e0ca58c62897540e6cf00089fed93a
eladraz49 <eladraz2008@gmail.com>
2024-11-26 19:32:41 +0200

    initial commit

---
+++ app/src/main/java/com/example/sharedprefrences/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.sharedprefrences;
+
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.sharedprefrences.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btn = findViewById(R.id.btn);
+        EditText etname = findViewById(R.id.etname);
+        TextView tv = findViewById(R.id.tv);
+        SharedPreferences sharedPreferences = getSharedPreferences("alert", MODE_PRIVATE);
+        tv.setText(sharedPreferences.getString("key", "Hello ###"));
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                String name = etname.getText().toString();
+                editor.putString("key", "Hello "+name);
+                editor.apply();
+
+
+
+
+            }
+        });
+    }
+}
